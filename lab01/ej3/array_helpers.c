c61e4e13bf998da281cc1d3a84a2248bad40b97b
Lozano-Benjamin <benjalozano13@outlook.com>
2022-03-24 00:09:17 -0300

    Ej3 modularizacion :)

---
+++ lab01/ej3/array_helpers.c
@@ -0,0 +1,38 @@
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "array_helpers.h"
+#define MAX_SIZE 100000
+
+
+unsigned int array_from_file(int array[], unsigned int max_size, const char *filepath) {
+    unsigned int len;
+    FILE * newFileD = fopen(filepath, "r");
+    if (newFileD == NULL){
+        fprintf(stderr, "Fallo en fopen");
+    }
+    fscanf(newFileD,"%u",&len);
+    max_size = MAX_SIZE;
+    printf("largo del arreglo: %d\n",len);
+    for (unsigned int i = 0; i < len || i < max_size ; i++){
+        fscanf(newFileD,"%d",&array[i]);
+    }
+    fclose(newFileD);
+
+    return len;
+     
+}
+
+void array_dump(int a[], unsigned int length) {
+    //your code here!!!!!
+    printf("[");
+    for (unsigned int i = 0; i < length; i++){
+        if ( i != length-1){
+            printf("%d, ", a[i]);
+        }else{
+            printf("%d]\n",a[i]);
+        }   
+    }
+}
+
