1d7831fb94dd38d428df4f980811d8cd8c822e2b
Lozano-Benjamin <benjalozano13@outlook.com>
2022-04-05 19:52:02 -0300

    Lab02 y alguna que otra cosa previa

--- lab02/ej2/main.c
+++ lab02/ej2/main.c
@@ -1,18 +1,21 @@
 /* First, the standard lib includes, alphabetically ordered */
 #include <assert.h>
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Then, this project's includes, alphabetically ordered */
+#include "array_helpers.h"
+#include "sort_helpers.h"
+#include "sort.h"
 
 /* Maximum allowed length of the array */
-#define MAX_SIZE 100000
+static const unsigned int MAX_SIZE = 100000u;
 
 void print_help(char *program_name) {
     /* Print the usage help of this program. */
     printf("Usage: %s <input file path>\n\n"
-           "Loads an array given in a file in disk and prints it on the screen."
-           "\n\n"
+           "Sort an array given in a file in disk.\n"
+           "\n"
            "The input file must have the following format:\n"
            " * The first line must contain only a positive integer,"
            " which is the length of the array.\n"
@@ -28,11 +31,8 @@ void print_help(char *program_name) {
 char *parse_filepath(int argc, char *argv[]) {
     /* Parse the filepath given by command line argument. */
     char *result = NULL;
-    // Program takes exactly two arguments
-    // (the program's name itself and the input-filepath)
-    bool valid_args_count = (argc == 2);
 
-    if (!valid_args_count) {
+    if (argc < 2) {
         print_help(argv[0]);
         exit(EXIT_FAILURE);
     }
@@ -42,51 +42,29 @@ char *parse_filepath(int argc, char *argv[]) {
     return result;
 }
 
-unsigned int array_from_file(int array[], unsigned int max_size, const char *filepath) {  
-    unsigned int len, elem;
-    max_size = MAX_SIZE;
-
-    printf("Porfavor Ingrese la longitud del array: ");
-    scanf("%u", &len);
-    for (unsigned int i = 0; i < len && i < max_size; i++){
-        printf("\nIngrese el valor que quiere ingresar en la posicion %u: ", i);
-        scanf("%u",&elem);
-        array[i] = elem;
-    }
-
-    FILE *fp = fopen(filepath, "r");
-    fclose(fp);
-
-    return len;
-}
-
-void array_dump(int a[], unsigned int length) {
-    //your code here!!!!!
-    printf("[");
-    for (unsigned int i = 0; i < length; i++){
-        if ( i != length-1){
-            printf("%d, ", a[i]);
-        }else{
-            printf("%d]\n",a[i]);
-        }   
-    }
-}
-
-
 int main(int argc, char *argv[]) {
     char *filepath = NULL;
 
     /* parse the filepath given in command line arguments */
     filepath = parse_filepath(argc, argv);
-    
+
     /* create an array of MAX_SIZE elements */
     int array[MAX_SIZE];
-    
+
     /* parse the file to fill the array and obtain the actual length */
     unsigned int length = array_from_file(array, MAX_SIZE, filepath);
-    
-    /*dumping the array*/
+
+    /* create a copy of the array, to do some checks later */
+    int copy[MAX_SIZE];
+    array_copy(copy, array, length);
+
+    /* show the ordered array in the screen */
     array_dump(array, length);
-    
+
+    /* check if it is sorted */
+    assert(array_is_sorted(array, length));
+
+    /* check if it is a permutation of original */
+    assert(array_is_permutation_of(copy, array, length));
     return EXIT_SUCCESS;
 }
